4e0a7dc7184acdead30e065a5d38c41a65cf1aaa
tanmaybhosale98 <tanmay.bhosale@finmeadows.com>
2025-06-24 15:08:46 +0530

    Program to find average of array

---
+++ arrayprogramming/com/array/programming/AverageOfArray.java
@@ -0,0 +1,34 @@
+package arrayprogramming.com.array.programming;
+
+//Write a program to find average of array. 
+public class AverageOfArray {
+
+
+      public static void main(String[] args) {
+        
+        int[] num={2,3,5,6,8,4};
+
+        int sum=0;
+
+        for(int i=0;i<num.length;i++){
+
+            sum=sum+num[i];
+        }
+
+  
+
+
+        float averageOfArray=(float)sum/num.length;
+
+        System.out.println("average of array is :"+averageOfArray);
+
+
+
+
+
+
+      }
+
+
+
+}
